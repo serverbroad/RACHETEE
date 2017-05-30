834bb42f6c71327a94aff42dd3d7744ff6355904
Okiki Continental <helpdesk@okiki-continental.com>
2017-05-30 13:27:50 +0000

    endingofmonthupdate

---
+++ build/generated/source/buildConfig/release/com/okikicontinental/rachetee/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.okikicontinental.rachetee;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.okikicontinental.rachetee";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 20540;
+  public static final String VERSION_NAME = "2.9";
+}
